d851415a014313000a257cfb76c6a5284d2ff0f4
Marium <mariumsana55@gmail.com>
2025-08-10 17:24:38 -0400

    Solved incorrect answer problems for some large inputs

--- src/main/java/ca/concordia/gamma/GammaFunction.java
+++ src/main/java/ca/concordia/gamma/GammaFunction.java
@@ -16,6 +16,28 @@ public class GammaFunction {
     }
 
     public static double gamma(double x) {
+        if (Double.isNaN(x) || Double.isInfinite(x)) {
+            return Double.NaN;
+        }
+        if (x <= 0 && x == floor(x)) {
+            return Double.NaN;
+        }
+        if (x > 170) {
+            return Double.POSITIVE_INFINITY;
+        }
+        // Special cases for accuracy
+        if (Math.abs(x - floor(x)) < 1e-10 && x > 0) {
+            return factorial(x - 1); // Gamma(n) = (n-1)!
+        }
+        if (Math.abs(x - 0.5) < 1e-10) {
+            return mySqrt(PI());
+        }
+        // Reflection formula for negative non-integers
+        if (x < 0.5) {
+            return PI() / (mySin(PI() * x) * gamma(1 - x));
+        }
+        // Lanczos approximation for positive x
+        x -= 1;
         double g = 7;
         double[] p = {
             0.99999999999980993, 676.5203681218851, -1259.1392167224028,
@@ -23,31 +45,30 @@ public class GammaFunction {
             -0.13857109526572012, 9.9843695780195716e-6,
             1.5056327351493116e-7
         };
-
-        if (x < 0.5) {
-            return GammaFunction.PI() / (GammaFunction.mySin(GammaFunction.PI() * x) * gamma(1 - x));
-        } else {
-            x -= 1;
-            double a = p[0];
-            for (int i = 1; i < p.length; i++) {
-                a += p[i] / (x + i);
-            }
-            double t = x + g + 0.5;
-            return GammaFunction.mySqrt(2 * GammaFunction.PI()) * GammaFunction.myPow(t, x + 0.5) * GammaFunction.myExp(-t) * a;
+        double a = p[0];
+        for (int i = 1; i < p.length; i++) {
+            a += p[i] / (x + i);
         }
+        double t = x + g + 0.5;
+        return mySqrt(2 * PI()) * myPow(t, x + 0.5) * myExp(-t) * a;
     }
 
-    // Manual implementations below
+    public static double factorial(double n) {
+        double result = 1.0;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 
     public static double PI() {
-        return 3.14159265358979323846;
+        return 3.141592653589793;
     }
 
     public static double mySin(double x) {
-        // Taylor series for sin(x), x in radians
         double term = x;
         double sum = x;
-        for (int i = 1; i < 15; i++) {
+        for (int i = 1; i < 30; i++) {
             term *= -x * x / ((2 * i) * (2 * i + 1));
             sum += term;
         }
@@ -57,7 +78,7 @@ public class GammaFunction {
     public static double myExp(double x) {
         double sum = 1.0;
         double term = 1.0;
-        for (int i = 1; i < 30; i++) {
+        for (int i = 1; i < 50; i++) {
             term *= x / i;
             sum += term;
         }
@@ -65,18 +86,19 @@ public class GammaFunction {
     }
 
     public static double myPow(double base, double exp) {
-        // pow(a, b) = exp(b * ln(a))
         return myExp(exp * myLn(base));
     }
 
     public static double myLn(double x) {
-        // Natural log using Newton's method
+        // Use a simple approximation for ln(x) for x > 0
         if (x <= 0) return Double.NaN;
-        double guess = 1.0;
-        for (int i = 0; i < 20; i++) {
-            guess = guess - (myExp(guess) - x) / myExp(guess);
+        double y = (x - 1) / (x + 1);
+        double y2 = y * y;
+        double sum = 0.0;
+        for (int n = 1; n < 100; n += 2) {
+            sum += (1.0 / n) * Math.pow(y, n);
         }
-        return guess;
+        return 2 * sum;
     }
 
     public static double mySqrt(double x) {
@@ -89,7 +111,6 @@ public class GammaFunction {
     }
 
     public static double floor(double x) {
-        int xi = (int)x;
-        return (x < xi) ? xi - 1 : xi;
+        return (double)((int)x - (x < 0 && x != (int)x ? 1 : 0));
     }
 }
